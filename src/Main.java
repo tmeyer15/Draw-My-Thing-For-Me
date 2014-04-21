23a723b1e5e28fd9a2b1331f116b32997ea2895e
John Mooring <jmooring@vt.edu>
2014-04-21 01:02:34 -0400

    fast drawing

--- src/Main.java
+++ src/Main.java
@@ -1,250 +1,132 @@
-import java.util.Set;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.MouseInfo;
 import java.awt.event.InputEvent;
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Robot;
-import java.io.IOException;
-import java.io.File;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.Image;
-import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.Toolkit;
-import java.awt.Dimension;
 
 
-public class Main
-{
-    final static int SIZE = 200;
+public class Main {
+    final static int SIZE = 100;
     static BufferedImage img = null;
-    public static void main(String[] args)
-    {
-        /*
-        Robot robo = null;
-        try
-        {
-           robo = new Robot();
-        }
-        catch (Exception e){}
+    private static ArrayList<Color> expectedColors = new ArrayList<Color>();
 
-        for(int asdf = 0; asdf < 100000; asdf++)
-        {
+    public static void main(String[] args) throws Exception {
 
-            Point p = MouseInfo.getPointerInfo().getLocation();
-            System.out.println(robo.getPixelColor(p.x, p.y));
 
-            robo.delay(2000);
-        }*/
-
-        //Pick the image we want to draw
         JFileChooser chooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
-            "Image File", "png", "jpg", "jpeg", "bmp", "gif");
+                "Image File", "png", "jpg", "jpeg", "bmp", "gif");
         chooser.setFileFilter(filter);
         int returnVal = chooser.showOpenDialog(null);
-        if(returnVal != JFileChooser.APPROVE_OPTION)
-        {
+        if (returnVal != JFileChooser.APPROVE_OPTION) {
             System.out.println("Bad file chosen");
             System.exit(1);
         }
-        String pictureFilePath = chooser.getSelectedFile().getAbsolutePath();
-        System.out.println("" + chooser.getSelectedFile().getAbsolutePath());
-        try
-        {
-            img = ImageIO.read(new File(pictureFilePath));
+
+        img = ImageIO.read(chooser.getSelectedFile());
+
+
+        expectedColors.add(new Color(109, 109, 109, 0));
+        expectedColors.add(new Color(7, 7, 7, 1));
+        expectedColors.add(new Color(255, 255, 255, 2));
+        expectedColors.add(new Color(222, 0, 22, 3));
+        expectedColors.add(new Color(67, 199, 0, 4));
+        expectedColors.add(new Color(0, 0, 255, 5));
+        expectedColors.add(new Color(155, 182, 255, 6));
+        expectedColors.add(new Color(238, 240, 0, 7));
+        expectedColors.add(new Color(238, 130, 0, 8));
+        expectedColors.add(new Color(79, 22, 132, 9));
+        expectedColors.add(new Color(226, 161, 244, 10));
+        expectedColors.add(new Color(206, 158, 109, 11));
+        expectedColors.add(new Color(75, 43, 10, 12));
+
+        int w = img.getWidth(), h = img.getHeight();
+        if (w > h) {
+            BufferedImage newImg = new BufferedImage(SIZE, SIZE * h / w, BufferedImage.TYPE_INT_ARGB);
+            newImg.getGraphics().drawImage(img.getScaledInstance(SIZE, SIZE * h / w, Image.SCALE_DEFAULT), 0, 0, null);
+            img = newImg;
+        } else {
+            BufferedImage newImg = new BufferedImage(SIZE * w / h, SIZE, BufferedImage.TYPE_INT_ARGB);
+            newImg.getGraphics().drawImage(img.getScaledInstance(SIZE * w / h, SIZE, Image.SCALE_DEFAULT), 0, 0, null);
+            img = newImg;
         }
-        catch (IOException e) {}
 
-        //Create the set of colors that we will use
-        HashSet<Color> expectedColors = new HashSet<Color>();
-        expectedColors.add(new Color(109,109,109));
-        expectedColors.add(new Color(  7,  7,  7));
-        expectedColors.add(new Color(255,255,255));
-        expectedColors.add(new Color(222,  0, 22));
-        expectedColors.add(new Color( 67,199,  0));
-        expectedColors.add(new Color(  0,  0,255));
-        expectedColors.add(new Color(155,182,255));
-        expectedColors.add(new Color(238,240,  0));
-        expectedColors.add(new Color(238,130,  0));
-        expectedColors.add(new Color( 79, 22,132));
-        expectedColors.add(new Color(226,161,244));
-        expectedColors.add(new Color(206,158,109));
-        expectedColors.add(new Color( 75, 43, 10));
+        Robot rob = new Robot();
 
-        //Resize the image into a square
-        BufferedImage newImg = new BufferedImage(SIZE,SIZE,BufferedImage.TYPE_INT_ARGB);
-        newImg.getGraphics().drawImage(img.getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT),0,0,null);
-        img = newImg;
 
-        /*
-        //Convert the image to grayscale and find the average gray value
-        int totalGrayscale = 0;
-        for (int x = 0; x < img.getWidth(); x++)
-        {
-            for (int y = 0; y < img.getHeight(); y++)
-            {
-                int rawColor = img.getRGB(x, y);
-                int grayscale = colorToGrayscale(rawColor);
-                int newColor = 0xFF;
-                newColor = (newColor << 8) | grayscale;
-                newColor = (newColor << 8) | grayscale;
-                newColor = (newColor << 8) | grayscale;
-                img.setRGB(x, y, newColor);
-                totalGrayscale += grayscale;
-            }
-        }
-        int averageGrayscale = totalGrayscale / (SIZE * SIZE);
-        */
+        final BufferedImage resImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-        //Initialize the robot and wait five seconds for the user to position the mouse
-        Robot rob = null;
-        try
-        {
-           rob = new Robot();
-        }
-        catch (Exception e){}
-        rob.delay(5000);
-
-        //Search the around the mouse pointer for the colors
+        Thread.sleep(3000);
         Point cursorStart = MouseInfo.getPointerInfo().getLocation();
 
-        System.out.println("Starting search");
-
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        final double width = screenSize.getWidth();
-        final double height = screenSize.getHeight();
-
-        HashMap<Color, Point> colorLocations = new HashMap<Color, Point>();
-        final int SEARCHSIZE = 400;
-        final int XSEARCHWIDTH = 120;
-        final int YSEARCHWIDTH = 20;
-        final int XCOLORWIDTH = 10;
-        for (int x = 0; x < width && colorLocations.size()<14; x+=XSEARCHWIDTH)
-        {
-            for (int y = 0; y < height && colorLocations.size()<14; y+=YSEARCHWIDTH)
-            {
-                Color col = rob.getPixelColor(x, y);
-                if (expectedColors.contains(col))
-                {
-                    int xi = x;
-                    col = rob.getPixelColor(xi, y);
-                    int missed = 0;
-                    while (missed<3)
-                    {
-                        if (expectedColors.contains(col))
-                        {
-                            missed = 0;
-                            colorLocations.put(col, new Point(xi, y));
-                        } else {
-                            missed++;
-                        }
-                        xi-=XCOLORWIDTH;
-                        col = rob.getPixelColor(xi, y);
-                    }
-                    xi = x;
-                    col = rob.getPixelColor(xi, y);
-                    missed = 0;
-                    while (missed<3)
-                    {
-                        if (expectedColors.contains(col))
-                        {
-                            missed = 0;
-                            colorLocations.put(col, new Point(xi, y));
-                        } else {
-                            missed++;
-                        }
-                        xi+=XCOLORWIDTH;
-                        col = rob.getPixelColor(xi, y);
-                    }
-                }
-
-            }
-        }
-
-        System.out.println("Ending search, " + colorLocations.size());
-
         //Draw the image
-        Color prevColor = null;
-        for (int x = 0; x < img.getWidth(); x+=3)
-        {
+        for (Color c : expectedColors) {
+            rob.mouseMove((int) (cursorStart.x + 12 * c.getAlpha() + 1), cursorStart.y);
+
+            rob.delay(5);
             rob.mousePress(InputEvent.BUTTON1_MASK);
+            rob.delay(5);
             rob.mouseRelease(InputEvent.BUTTON1_MASK);
-            for (int y = 0; y < img.getHeight(); y+=3)
-            {
-                /*
-                int color = (0xFF & img.getRGB(x,y));
-                if (color < averageGrayscale)
-                {
-                    rob.mousePress(InputEvent.BUTTON1_MASK);
-                    rob.mouseRelease(InputEvent.BUTTON1_MASK);
-                    rob.mouseMove(cursorStart.x + x, cursorStart.y + y);
-                    //rob.delay(2);
-                }*/
-                Color nearestColor = getNearestColor(colorLocations.keySet(), new Color(img.getRGB(x, y)));
-                rob.mouseMove(cursorStart.x + x, cursorStart.y + y);
-                if (nearestColor != null)
-                {
-                    if (!nearestColor.equals(prevColor))
-                    {
-                        rob.mouseMove(cursorStart.x + x, cursorStart.y + y);
-                        rob.mouseRelease(InputEvent.BUTTON1_MASK);
 
-                        //Move mouse to color and select it
-                        Point colorPos = colorLocations.get(nearestColor);
-                        rob.mouseMove(colorPos.x, colorPos.y);
+
+            for (int y = 0; y < img.getHeight(); y += 1) {
+                for (int x = 0; x < img.getWidth(); x += 1) {
+                    Color nearestColor = getNearestColor(img.getRGB(x, y));
+                    if (nearestColor == c && c.getAlpha()==1) {
+
+                        resImage.setRGB(x, y, (nearestColor.getRGB() & 0x00ffffff) | 0xff000000);
+
+
+                        rob.mouseMove(cursorStart.x + x * 3, cursorStart.y - 320 + y * 3);
+
                         rob.mousePress(InputEvent.BUTTON1_MASK);
                         rob.mouseRelease(InputEvent.BUTTON1_MASK);
-                        rob.delay(4);
 
-                        //Move mouse to where we are drawing and draw
-                        rob.mouseMove(cursorStart.x + x, cursorStart.y + y);
-                        rob.mousePress(InputEvent.BUTTON1_MASK);
-                        rob.delay(4);
-
-                        prevColor = nearestColor;
                     }
                 }
             }
         }
         rob.mouseRelease(InputEvent.BUTTON1_MASK);
         //Display the image in a frame
-        JFrame gui = new JFrame()
-        {
-            public void paint(Graphics g)
-            {
-                g.drawImage(img, 0, 0, null);
+        JFrame gui = new JFrame() {
+            public void paint(Graphics g) {
+                g.drawImage(resImage, 0, 0, null);
+                try {
+                    Thread.sleep(500);
+                    repaint();
+                } catch (Exception ignore) {
+                }
             }
         };
         gui.setVisible(true);
         gui.setSize(SIZE, SIZE);
+        gui.setResizable(false);
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private static int colorToGrayscale(int color)
-    {
+    private static int colorToGrayscale(int color) {
         //Use the eye's intensity for RGB to convert to grayscale
-        int grayscale = 0;
-        grayscale += ((0x00FF0000 & color) >> 16) * 0.3;
-        grayscale += ((0x0000FF00 & color) >> 8)  * 0.59;
-        grayscale += ((0x000000FF & color) >> 0)  * 0.11;
-        return grayscale;
+        int grayScale = 0;
+        grayScale += ((0xff0000 & color) >> 16) * 0.3;
+        grayScale += ((0xff00 & color) >> 8) * 0.59;
+        grayScale += (0xff & color) * 0.11;
+        return grayScale;
     }
 
-    private static Color getNearestColor(Set<Color> colors, Color color)
-    {
+    private static Color getNearestColor(int color) {
         //returns the color in the set that is closest to the color
         Color closestColor = null;
         double closestDistance = Double.MAX_VALUE;
-        for (Color c: colors)
-        {
+        for (Color c : expectedColors) {
             double dist = colorDistance(c, color);
-            if (dist < closestDistance)
-            {
+            if (dist < closestDistance) {
                 closestColor = c;
                 closestDistance = dist;
             }
@@ -252,9 +134,15 @@ public class Main
         return closestColor;
     }
 
-    private static double colorDistance(Color c1, Color c2)
-    {
-        return Math.sqrt( 0.3 * Math.pow(c1.getRed() - c2.getRed(),2.0) + 0.59 * Math.pow(c1.getGreen() - c2.getGreen(),2.0) + 0.11 * Math.pow(c1.getBlue() - c2.getBlue(),2.0));
-        //return Math.sqrt(Math.pow(c1.getRed() - c2.getRed(),2.0) + Math.pow(c1.getGreen() - c2.getGreen(),2.0) + Math.pow(c1.getBlue() - c2.getBlue(),2.0));
+    private static double colorDistance(Color c1, int c2) {
+        float[] hsb1 = Color.RGBtoHSB(c1.getRed(), c1.getGreen(), c1.getBlue(), null);
+        float[] hsb2 = Color.RGBtoHSB((c2 >> 16) & 0xff, (c2 >> 8) & 0xff, c2 & 0xff, null);
+
+        float dh = hsb1[0] - hsb2[0];
+        float ds = hsb1[1] - hsb2[1];
+        float dv = hsb1[2] - hsb2[2];
+
+        return dh * dh + ds * ds + dv * dv;
+
     }
 }
